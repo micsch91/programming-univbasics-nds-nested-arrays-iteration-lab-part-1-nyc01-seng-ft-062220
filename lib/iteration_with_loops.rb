498db387441b62f4be6dc0094556f358d7e4ac7c
Michael Schneider <mic.sch91@gmail.com>
2020-06-03 15:18:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,18 +12,3 @@ end
 row_index += 1
 end
 end
-spice_rack = [
-  ["Posh", "Scary", "Sporty"],
-  ["Paprika", "Fajita Mix", "Coriander"],
-  ["Parsley", "Sage", "Rosemary"]
-]
-
-row_index = 0
-while row_index < spice_rack.count do
-  element_index = 0
-  while element_index < spice_rack[row_index].count do
-    p spice_rack[row_index][element_index]
-    element_index += 1
-  end
-  row_index += 1
-end #=> nil
